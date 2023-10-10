e154edc2bb6676d81d2c1f76dca68e7eaa57205c
suhdonghwi <hwidongsuh@gmail.com>
2023-10-10 11:18:54 +0900

    update auto-session

--- private_dot_config/nvim/lua/config/auto-session.lua
+++ private_dot_config/nvim/lua/config/auto-session.lua
@@ -1,1 +1,10 @@
-require("auto-session").setup({})
+local function close_neo_tree()
+	require("neo-tree.sources.manager").close_all()
+end
+
+require("auto-session").setup({
+	bypass_session_save_file_types = { "neo-tree" },
+	pre_save_cmds = {
+		close_neo_tree,
+	},
+})
