320e17ae6905e38da2493b4ccf7be3b66702c859
서동휘 <hwidongsuh@gmail.com>
2024-05-24 01:24:08 +0900

    update

--- private_dot_config/nvim/lua/config/trouble.lua
+++
@@ -1,9 +0,0 @@
-local wk = require("which-key")
-
-wk.register({
-	d = { ":Trouble<CR>", "Open the trouble list" },
-}, { prefix = "<leader>" })
-
-require("trouble").setup({
-	use_diagnostic_signs = true,
-})
