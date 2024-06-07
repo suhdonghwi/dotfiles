fc7e39e0dc856e69a5425c4f22568637a46bec04
서동휘 <hwidongsuh@gmail.com>
2024-06-07 21:52:18 +0900

    update

--- private_dot_config/nvim/lua/config/auto-session.lua
+++ private_dot_config/nvim/lua/config/auto-session.lua
@@ -1,4 +1,7 @@
 require("auto-session").setup({
 	bypass_session_save_file_types = { "NvimTree" },
-	pre_save_cmds = { "NvimTreeClose" },
+	pre_save_cmds = {
+	  "NvimTreeClose",
+    "lua for _, view in ipairs(require('diffview.lib').views) do view:close() end"
+	},
 })
