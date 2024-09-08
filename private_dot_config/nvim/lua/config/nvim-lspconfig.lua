e25460f3898c5685dee00a864b36570a0b1b4b86
Suh Donghwi <hwidongsuh@gmail.com>
2024-09-08 18:31:14 +0900

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/nvim-lspconfig.lua

--- private_dot_config/nvim/lua/config/nvim-lspconfig.lua
+++ private_dot_config/nvim/lua/config/nvim-lspconfig.lua
@@ -10,6 +10,37 @@ local lsp_defaults = lspconfig.util.default_config
 lsp_defaults.capabilities =
 	vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
 
+local async_formatting = function()
+	local bufnr = vim.api.nvim_get_current_buf()
+
+	vim.lsp.buf_request(
+		bufnr,
+		"textDocument/formatting",
+		vim.lsp.util.make_formatting_params({}),
+		function(err, res, ctx)
+			if err then
+				local err_msg = type(err) == "string" and err or err.message
+				-- you can modify the log message / level (or ignore it completely)
+				vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+				return
+			end
+
+			-- don't apply results if buffer is unloaded or has been modified
+			if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+				return
+			end
+
+			if res then
+				local client = vim.lsp.get_client_by_id(ctx.client_id)
+				vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+				vim.api.nvim_buf_call(bufnr, function()
+					vim.cmd("silent noautocmd update")
+				end)
+			end
+		end
+	)
+end
+
 wk.add({
 	{
 		"<leader><leader>",
@@ -18,44 +49,43 @@ wk.add({
 		end,
 		desc = "Hover",
 	},
-  {
-    "<leader>l",
-    group = "LSP",
-  },
-  {
-    "<leader>la",
-    function()
-      vim.lsp.buf.code_action()
-    end,
-    desc = "Code Action",
-  },
-  {
-    "<leader>lr",
-    function()
-      vim.lsp.buf.rename()
-    end,
-    desc = "Rename",
-  },
-  {
-    "<leader>ld",
-    function()
-      vim.diagnostic.open_float()
-    end,
-    desc = "Show line diagnostics",
-  },
-  {
-    "<leader>lf",
-    function()
-      vim.lsp.buf.format()
-      vim.api.nvim_command("write")
-    end,
-    desc = "Format buffer",
-  },
-  {
-    "<leader>lo",
-    ":TSToolsOrganizeImports<CR>",
-    desc = "Organize imports (TypeScript specific)",
-  },
+	{
+		"<leader>l",
+		group = "LSP",
+	},
+	{
+		"<leader>la",
+		function()
+			vim.lsp.buf.code_action()
+		end,
+		desc = "Code Action",
+	},
+	{
+		"<leader>lr",
+		function()
+			vim.lsp.buf.rename()
+		end,
+		desc = "Rename",
+	},
+	{
+		"<leader>ld",
+		function()
+			vim.diagnostic.open_float()
+		end,
+		desc = "Show line diagnostics",
+	},
+	{
+		"<leader>lf",
+		function()
+			async_formatting()
+		end,
+		desc = "Format buffer",
+	},
+	{
+		"<leader>lo",
+		":TSToolsOrganizeImports<CR>",
+		desc = "Organize imports (TypeScript specific)",
+	},
 })
 
 -- Python
